096db13c412e82c8fb1f876bedabbd2a9e6b1d6d
Nassim <nassimcharaabi581@gmail.com>
2025-10-07 20:31:38 +0200

    first change

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
-from models import db
+from flask import Flask, request, redirect, render_template
+from models import db, User
+from werkzeug.security import generate_password_hash
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///smart_budget.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
-with app.app_context():
-    db.create_all()
-    print("Database and tables created!")
+@app.route('/register', methods=['POST'])
+def register():
+    first_name = request.form['firstName']
+    last_name = request.form['lastName']
+    email = request.form['email']
+    password = request.form['password']
+    currency = request.form['currency']
+    monthly_budget = request.form.get('monthlyBudget', 0.0)
+
+    hashed_password = generate_password_hash(password, method='sha256')
+    new_user = User(first_name=first_name, last_name=last_name,
+                    email=email, password=hashed_password,
+                    currency=currency, monthly_budget=monthly_budget)
+
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect('/login')
